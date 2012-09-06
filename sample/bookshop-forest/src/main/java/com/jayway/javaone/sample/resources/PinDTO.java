d2ea5197bc9d1c094729f79946c599b038d0bf9e
Jan Kronquist <jan.kronquist@gmail.com>
2012-09-06 15:32:33 +0200

    Refactored after first practice

---
+++ sample/bookshop-forest/src/main/java/com/jayway/javaone/sample/resources/PinDTO.java
@@ -0,0 +1,13 @@
+package com.jayway.javaone.sample.resources;
+
+public class PinDTO {
+	private String pin;
+
+	public String getPin() {
+		return pin;
+	}
+
+	public void setPin(String pin) {
+		this.pin = pin;
+	}
+}
